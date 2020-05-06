edc77a285aaacb45530860ffc8a88cc69f60ceef
Andrew <pemberton5150@gmail.com>
2020-05-05 21:46:29 -0600

    Updates

---
+++ precache-manifest.66bd85a9689c709d920b63b0843fafda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdcad003b06caffdc13e008d7abf89f6",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "6516c05dcb761ca3ef24",
+    "url": "/clicky-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d26cfe00fd66058ef140",
+    "url": "/clicky-game/static/js/2.29d63bea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.29d63bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6516c05dcb761ca3ef24",
+    "url": "/clicky-game/static/js/main.bc3d3aad.chunk.js"
+  },
+  {
+    "revision": "e6346a5c09dc06c593a5",
+    "url": "/clicky-game/static/js/runtime-main.ca4d9b5e.js"
+  }
+]);
